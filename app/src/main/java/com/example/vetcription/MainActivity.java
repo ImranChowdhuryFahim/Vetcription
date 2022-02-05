e019c4939e8091744f36a7680e94b8b082b7c2ac
eamon831 <eamon831@gmail.com>
2022-02-06 00:56:02 +0600

    added new log in page press log in button on older log in page

--- app/src/main/java/com/example/vetcription/MainActivity.java
+++ app/src/main/java/com/example/vetcription/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         Login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this,VetDataShow.class));
+                startActivity(new Intent(MainActivity.this,New_login.class));
             }
         });
     }
---
+++ app/src/main/java/com/example/vetcription/New_login.java
@@ -0,0 +1,47 @@
+package com.example.vetcription;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.vetcription.R;
+
+public class New_login extends AppCompatActivity {
+    EditText user_name,password;
+    Button forget_password,log_in,sign_up_doctor,sign_up_user;
+    ConstraintLayout root;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_login);
+        //assign
+        user_name=findViewById(R.id.username);
+        password=findViewById(R.id.password);
+        forget_password=findViewById(R.id.forget_password);
+        log_in=findViewById(R.id.log_in);
+        sign_up_doctor=findViewById(R.id.sign_up_doctor);
+        sign_up_user=findViewById(R.id.sign_up_user);
+        root=findViewById(R.id.root);
+        animation();
+    }
+
+    private void animation() {
+        Animation leftoright= AnimationUtils.loadAnimation(New_login.this,R.anim.lefttoright);
+        Animation righttoleft=AnimationUtils.loadAnimation(New_login.this,R.anim.righttoleft);
+        Animation bottomtoup=AnimationUtils.loadAnimation(New_login.this,R.anim.bottomtotop);
+        Animation alpha=AnimationUtils.loadAnimation(New_login.this,R.anim.visibility);
+        root.setAnimation(alpha);
+        user_name.setAnimation(leftoright);
+        password.setAnimation(righttoleft);
+        forget_password.setAnimation(leftoright);
+        log_in.setAnimation(righttoleft);
+        sign_up_user.setAnimation(bottomtoup);
+        sign_up_doctor.setAnimation(bottomtoup);
+    }
+}
