85cbb094224699a0bc5a007665d1d545df6ae4ff
Emon <eamon831>
2022-05-14 08:51:25 -0700

    Single Login completed

--- app/src/main/java/com/example/vetcription/New_login.java
+++ app/src/main/java/com/example/vetcription/New_login.java
@@ -1,10 +1,12 @@
 package com.example.vetcription;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -12,10 +14,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class New_login extends AppCompatActivity {
     EditText user_name,password;
     Button forget_password,log_in,sign_up_doctor,sign_up_user;
     ConstraintLayout root;
+    private FirebaseAuth mauth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,9 +37,48 @@ public class New_login extends AppCompatActivity {
         sign_up_doctor=findViewById(R.id.sign_up_doctor);
         sign_up_user=findViewById(R.id.sign_up_user);
         root=findViewById(R.id.root);
+        mauth=FirebaseAuth.getInstance();
         log_in.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                String email,pass;
+                email=user_name.getText().toString();
+                pass=password.getText().toString();
+                int tf=0;
+
+                if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                    tf=1;
+                    user_name.setError("Invalid email");
+                    user_name.requestFocus();
+                }
+                if(pass.length()<6)
+                {
+                    password.setError("Invalid Password");
+                    password.requestFocus();
+                    tf++;
+                }
+                if(tf>0)
+                {
+                    return;
+                }
+
+                mauth.signInWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+
+
+                            startActivity(new Intent(New_login.this, New_doc_dashboard.class));
+                            finish();
+                        }else{
+
+                            Toast.makeText(New_login.this, "Invalid credentials", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+
 
                 String value=password.getText().toString();
 
